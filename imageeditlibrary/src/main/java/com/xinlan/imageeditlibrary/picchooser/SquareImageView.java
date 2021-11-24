19432758568a1bdfd880beec43a15b552967d9a8
Pengfei Wang <pengfei.wang@anker.com>
2021-11-24 18:10:52 +0800

    项目初始化

---
+++ imageeditlibrary/src/main/java/com/xinlan/imageeditlibrary/picchooser/SquareImageView.java
@@ -0,0 +1,22 @@
+package com.xinlan.imageeditlibrary.picchooser;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+
+/**
+ * 正方形的ImageView  强制 宽度 = 高度
+ *
+ */
+public class SquareImageView extends androidx.appcompat.widget.AppCompatImageView {
+
+    public SquareImageView(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth());
+    }
+}//end class
